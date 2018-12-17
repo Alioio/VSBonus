7e5353314a078fd29a78f5a0c17dd92efc3c615f
Alioio <progmaster11gmail.com>
2018-12-17 15:56:13 +0100

    init client part

---
+++ WeatherServer/weather/src/main/java/de/htw/ai/vs/weather/weather/response/response.java
@@ -0,0 +1,5 @@
+package de.htw.ai.vs.weather.weather.response;
+
+public class response {
+
+}
--- WeatherServer/weather/src/main/java/de/htw/ai/vs/weather/weather/server/WeatherServer.java
+++ WeatherServer/weather/src/main/java/de/htw/ai/vs/weather/weather/server/WeatherServer.java
@@ -1,12 +1,48 @@
 package de.htw.ai.vs.weather.weather.server;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class WeatherServer {
 
     public static void main(String[] args) {
-        System.out.println("Hello weather");
+        System.out.println("Hello weather server");
         
-        // start server
+        ////// TEST 
+
+        try {
+			startServer();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        ////////
         
+    }
+    
+    public static void startServer() throws IOException{
+    	
+    	String clientSentence = "";
+    	  String capitalizedSentence = "";
+    	  ServerSocket welcomeSocket = new ServerSocket(6789);
+
+    	  while (true) {
+    	   Socket connectionSocket = welcomeSocket.accept();
+    	   BufferedReader inFromClient =
+    	    new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+    	   DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+    	   clientSentence = inFromClient.readLine();
+    	   System.out.println("Received: " + clientSentence);
+    	   capitalizedSentence = clientSentence.toUpperCase() + 'n';
+    	   outToClient.writeBytes(capitalizedSentence);
+    	  }
+
+    	
     }
     
     // start server 
